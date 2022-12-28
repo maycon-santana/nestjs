6334ad9f55d6e08823b6b2e7c2ddb2a3d8701c17
maycon-santana <maycon.santana@thstec.com.br>
2022-12-27 22:19:52 -0300

    Criando o método de buscar por ID

--- iniciando-nestjs/src/cats/cats.controller.ts
+++ iniciando-nestjs/src/cats/cats.controller.ts
@@ -1,5 +1,5 @@
 /* eslint-disable prettier/prettier */
-import { Controller, Req, Get, Post } from '@nestjs/common';
+import { Controller, Req, Get, Post, Param } from '@nestjs/common';
 import { Request } from 'express';
 
 @Controller('cats')
@@ -13,4 +13,11 @@ export class CatsController {
     findAll(@Req() request: Request): string {
         return 'Este ação retona todos os gatos';
     }
+
+    @Get(':id')
+    findOne(@Param() params): string {
+        console.log(params.id);
+        return `Esta ação retorna o ID: #${params.id} de gatos`;
+    }
+
 }
